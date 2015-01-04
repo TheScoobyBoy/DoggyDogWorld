735180e02b6f47d3d290e82b630885e747f1f917
Alberto Cuenca <alberto@trovit.com>
2015-01-04 21:54:54 +0100

    DoggyDogWorld

---
+++ DoggyDogWorld/app/src/main/java/com/trovit/alberto/doggydogworld/Dog.java
@@ -0,0 +1,16 @@
+package com.trovit.alberto.doggydogworld;
+
+/**
+ * Created by alberto on 04/01/15.
+ */
+public class Dog {
+    String name;
+    int age;
+    int numberOfLegs = 4;
+    String breed;
+
+
+    String bark(){
+        return name + " barked";
+    }
+}
